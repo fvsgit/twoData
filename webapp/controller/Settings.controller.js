fbd0f40dd5a27e13bf298de101bfc43f2f7ea2de
fvsgit <francoisvans@gmail.com>
2017-12-12 22:55:12 +0000

    started with layout of settings page

--- webapp/controller/Settings.controller.js
+++ webapp/controller/Settings.controller.js
@@ -1,12 +1,41 @@
 sap.ui.define([
-	"twodata/controller/BaseController"
-], function(BaseController) {
+	"twodata/controller/BaseController",
+	"sap/m/GroupHeaderListItem"
+], function(BaseController, GroupHeaderListItem) {
 	"use strict";
 
-	return BaseController.extend("twodata.controller.Settings", { 
-		
-		onNavBack: function(){
+	return BaseController.extend("twodata.controller.Settings", {
+
+		onInit: function() {
+
+			//Attach the pattern matched event
+			this.getRouter().getRoute("settings").attachPatternMatched(this.onObjectMatched, this);
+
+		},
+		onObjectMatched: function(oEvent) {
+			// //Get the list
+			// var oList = this.getView().byId("lstSettings");
+			// //Get the master data mmodel
+			// var oModel = this.getModel("masterData").getData();
+			// //Check that the model is not undefined
+			// if(oModel !== undefined){
+			// 	for(var i=0;i<oModel.Settings.length;i++){
+			// 		this.getView().addContent(new sap.m.Label({ text: "test"}));
+			// 		this.getView().addContent(new sap.ui.core.SeparatorItem());
+			// 		// var oNewListItem = new sap.m.CustomListItem({ title: oModel.Settings[i].Title, description: oModel.Settings[i].Subtitle });
+			// 		// oNewListItem.addContent(new sap.m.Switch({ state: true }));
+			// 		// oList.addItem(oNewListItem);
+			// 	}
+			// }
+		},
+		onNavBack: function() {
 			this.navigateBack();
+		},
+		getGroupHeader: function(oGroup) {
+			return new GroupHeaderListItem({
+				title: oGroup.key,
+				upperCase: true
+			});
 		}
 	});
 
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -11,8 +11,8 @@ sap.ui.define([
 			oModel.setDefaultBindingMode("OneWay");
 			return oModel;
 		},
-		
-		createMasterDataModel: function(){
+
+		createMasterDataModel: function() {
 			var oModel = new JSONModel({
 				"Companies": [{
 					"Id": 1,
@@ -22,14 +22,50 @@ sap.ui.define([
 					"Tasks": 4,
 					"State": "Error",
 					"Info": "Outstanding Tasks"
-				},{
+				}, {
 					"Id": 2,
 					"Name": "Carrus",
 					"Description": "Logistics",
 					"Icon": "sap-icon://shipping-status",
 					"Tasks": 0,
 					"State": "Success",
-					"Info": "Done" 
+					"Info": "Done"
+				}],
+				"Settings": [{
+					"Title": "Screen Orientation",
+					"Subtitle": "Force the application to use a specific screen orientation",
+					"Value": "Portrait",
+					"Options": ["Portrait", "Landscape"],
+					"Group": "General",
+					"SortOder": 1
+				}, {
+					"Title": "History",
+					"Subtitle": "Duration the history should be kept for",
+					"Value": "Forever",
+					"Options": ["1 Week", "1 Month", "Forever"],
+					"Group": "General",
+					"SortOder": 1
+				}, {
+					"Title": "Theme",
+					"Subtitle": "Change the look of the application",
+					"Value": "Light",
+					"Options": ["Light", "Dark"],
+					"Group": "General",
+					"SortOder": 1
+				}, {
+					"Title": "Sync only on Wi-Fi",
+					"Subtitle": "Reduces data usage",
+					"Value": true,
+					"Options": [true, false],
+					"Group": "Data Usage",
+					"SortOder": 2
+				}, {
+					"Title": "Auto-Sync",
+					"Subtitle": "Automatically sync data when there is a network connection",
+					"Value": true,
+					"Options": [true, false],
+					"Group": "Data Usage",
+					"SortOder": 2
 				}]
 			});
 			return oModel;
